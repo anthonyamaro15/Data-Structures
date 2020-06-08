20574276c04d9501694d0729fb3b8d78a64b37f7
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-06-08 14:06:36 -0700

    done with stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -10,23 +10,55 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when
    implementing a Stack?
 """
-from doubly_linked_list import ListNode
+from singly_linked_list import *
+
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.extend([value])
+
+#     def pop(self):
+#         if len(self.storage) > 0:
+#             value = self.storage[-1]
+#             self.storage.pop()
+
+#             return value
 
 
 class Stack:
-    def __init__(self, storage=[]):
-        self.size = 0
-        self.storage = storage
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.storage = LinkedList()
 
     def __len__(self):
-        return len(self.storage)
+        temp = self.head
+        count = 0
+        while (temp):
+            count += 1
+            temp = temp.next
+        return count
 
     def push(self, value):
-        self.storage.append(value)
-        self.size = + 1
-
-    def pop(self, value):
-        if self.size != 0:
-            self.storage.pop(value)
+        if self.head is None:
+            self.head = Node(value)
         else:
-            return
+            newNode = Node(value)
+            newNode.next = self.head
+            self.head = newNode
+
+    def pop(self):
+        if self.head is None:
+            return None
+        else:
+            popped = self.head
+            self.head = self.head.next
+            popped.next = None
+            return popped.value
