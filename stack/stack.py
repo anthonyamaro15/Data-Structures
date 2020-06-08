c28319bea795767cc69a9e3d3c4667a13f243fbb
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-06-07 17:10:29 -0700

    missing 2 test for stack

--- stack/stack.py
+++ stack/stack.py
@@ -1,28 +1,32 @@
 """
 A stack is a data structure whose primary purpose is to store and
-return elements in Last In First Out order. 
+return elements in Last In First Out order.
 
 1. Implement the Stack class using an array as the underlying storage structure.
    Make sure the Stack tests pass.
 2. Re-implement the Stack class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Stack tests pass.
-3. What is the difference between using an array vs. a linked list when 
+3. What is the difference between using an array vs. a linked list when
    implementing a Stack?
 """
+from doubly_linked_list import ListNode
 
 
 class Stack:
-    def __init__(self):
+    def __init__(self, storage=[]):
         self.size = 0
-        # self.storage = ?
+        self.storage = storage
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size = + 1
 
-    def pop(self):
-        pass
-
+    def pop(self, value):
+        if self.size != 0:
+            self.storage.pop(value)
+        else:
+            return
