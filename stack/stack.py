3978d74efbe1a15e3edd41f1225c72f1c8847232
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-06-08 19:38:46 -0700

    finished

--- stack/stack.py
+++ stack/stack.py
@@ -41,20 +41,25 @@ class Stack:
     def __len__(self):
         temp = self.head
         count = 0
+      #   if count temp is > 1 then while loop will run and add one to count
         while (temp):
             count += 1
             temp = temp.next
+            # return the count. to see how many items are in the node
         return count
 
     def push(self, value):
+      #  if head is empty then we will add a new node to it
         if self.head is None:
             self.head = Node(value)
         else:
             newNode = Node(value)
+            # else we add another node and call next
             newNode.next = self.head
             self.head = newNode
 
     def pop(self):
+      #   if head is empty we just return None
         if self.head is None:
             return None
         else:
